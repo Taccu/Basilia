00795940f4357d34847cdb98409f0a02c91d742d
Taccu <HolyFaith@web.de>
2015-08-30 14:51:19 +0100

    AIDSFEST

--- www/js/dbconnector.js
+++ www/js/dbconnector.js
@@ -1,22 +1,51 @@
-//Read database from file
-var fs = require('fs');
-var SQL = requre('sql.js);
-var filebuffer = fs.readFileSync('basilia.sqlite');
-//Load database
-var db = new SQL.Database(filebuffer);
-//Get Information from the database
-var getViaId = db.prepare("SELECT * FROM standorte WHERE id =:id");
-var getViaPNumber = db.prepare("SELECT * FROM standorte WHERE pNumber =:pNumber");
-
-
-//Example 
-var result = stmt.getAsObject({':id' : 1});
-console.log(result);
-
-//Reuse stmt
-stmt.bind([2]);
-while(stmt.step()) console.log(stmt.get());
-
-
-// Wenn fertig mit stmt 
-stmt.free();
+$(function(){ 
+	var db = {
+		init: function () {
+			this.initDatabase();
+		},
+		initDatabase: function() {
+			//Read database from file
+		var fs = require('fs');
+		var SQL = requre('sql.js');
+		var filebuffer = fs.readFileSync('basilia.sqlite');
+		//Load database
+		db = new SQL.Database(filebuffer);
+		//Get Information from the database
+		var getViaId = db.prepare("SELECT * FROM standorte WHERE id =:id");
+		var getViaPNumber = db.prepare("SELECT * FROM standorte WHERE pNumber =:pNumber");
+		},
+		/***
+		**** SELECT DATA **
+		***/	    
+	    selectAll: function() {
+	    	var that = this;
+			basilia.transaction(
+			    function (transaction) {
+			        transaction.executeSql("SELECT * FROM standorte;", [], that.dataSelectHandler, that.errorHandler);
+			    }
+			);			    
+	    },
+	    
+		dataSelectHandler: function(transaction, results){
+		 
+			// Handle the results
+			for (var i=0; i<results.rows.length; i++) {
+		 
+				var row = results.rows.item(i);
+				var newFeature = new Object();
+		 
+				newFeature.fid   = row['id'];
+				newFeature.fname = row['name'];
+				newFeature.fcolor = row['color'];
+				newFeature.fdesc  = row['description'];
+		 
+				$('body').css('background-color',newFeature.bgcolor);
+				$('body').css('font-family',newFeature.font);
+				$('#content list').html('<h4 id="bas">'+ newFeature.fname +'</h4>');
+			}
+		}
+	};
+ 	//Instantiate Demo
+ 	db.init();
+	
+});	
