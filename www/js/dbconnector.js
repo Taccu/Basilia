5b9d0a6e4c1f9dd1320a86da13821a8a2041a180
Taccu <HolyFaith@web.de>
2015-09-01 17:25:18 +0100

    sad

--- www/js/dbconnector.js
+++ www/js/dbconnector.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 $(function(){ 
     var db = {
@@ -30,3 +31,57 @@ $(function(){
     db.renderResults();
 
 });	
+=======
+//$(function(){ 
+	var db = {
+		init: function () {
+			this.initDatabase();
+		},
+		initDatabase: function() {
+			//Read database from file
+		var fs = require(['fs']);
+		var SQL = require(['sql.js']);
+		var filebuffer = fs.readFile('basilia.sqlite');
+		//Load database
+		db = new SQL.Database(filebuffer);
+		//Get Information from the database
+		var getViaId = db.prepare("SELECT * FROM standorte WHERE id =:id");
+		var getViaPNumber = db.prepare("SELECT * FROM standorte WHERE pNumber =:pNumber");
+		selectAll();
+		},
+		/***
+		**** SELECT DATA **
+		***/	    
+	    selectAll: function() {
+	    	var that = this;
+			basilia.transaction(
+			    function (transaction) {
+			        transaction.executeSql("SELECT * FROM standorte;", [], that.dataSelectHandler, that.errorHandler);
+			    }
+			);			    
+	    },
+	    
+		dataSelectHandler: function(transaction, results){
+		 
+			// Handle the results
+			for (var i=0; i<results.rows.length; i++) {
+		 
+				var row = results.rows.item(i);
+				var newFeature = new Object();
+		 
+				newFeature.fid   = row['id'];
+				newFeature.fname = row['name'];
+				newFeature.fcolor = row['color'];
+				newFeature.fdesc  = row['description'];
+		 
+				$('body').css('background-color',newFeature.bgcolor);
+				$('body').css('font-family',newFeature.font);
+				$('.list').html('<h4 id="bas">'+ newFeature.fname +'</h4>');
+			}
+		}
+	};
+ 	//Instantiate
+ //	db.init();
+	
+//});	
+>>>>>>> f845a0a7d4efb18a18810fe2f1064eea261bc762
